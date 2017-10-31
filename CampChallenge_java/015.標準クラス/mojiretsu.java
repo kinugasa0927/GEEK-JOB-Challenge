2d191b51517c26d8422ee0ee436b910813587afd
MYCOMPUTER\kinu <kinugasa.k0927@gmail.com>
2017-10-31 11:35:29 +0900

    課題を提出します

---
+++ CampChallenge_java/015.標準クラス/mojiretsu.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author kinu
+ */
+public class mojiretsu {
+    
+    public static void main(String[]args){
+        String name = "衣笠";
+        String address = "埼玉県";
+        
+        System.out.println(name.substring(0));
+        System.out.println(address.substring(0,2)+"に住んでいます");//0~2まで表示
+    }
+    
+}
---
+++ CampChallenge_java/015.標準クラス/mojiretsu3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author kinu
+ */
+public class mojiretsu3 {
+    public static void main(String[]args){
+        String word = "きょUはぴIえIちぴIのくみこみかんすUのがくしゅUをしてIます";
+        
+        System.out.println(word.replace("U","う").replace("I","い"));
+        
+    }
+}
+
+
